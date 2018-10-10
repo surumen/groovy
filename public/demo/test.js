315e99b4ed077fe8fbab30894e05fbb8b912ceb0
Moses Surumen <moses@The-Black-Panther.local>
2018-10-09 20:51:37 -0700

    initial commit

---
+++ public/demo/test.js
@@ -0,0 +1,7 @@
+require('../')('./demo/tamia.mp3', {
+  buffer: false
+}).on('error', function (err) {
+  console.error('GOT ERROR', err.message)
+}).on('load', function () {
+  console.log('GOT LOAD')
+})
